9dda71d5201fbfd99be3f2d2b95456d6f26eb136
gandalf <ng_gabe@hotmail.com>
2025-05-07 15:16:25 +0800

    touched up shadows

--- Assets/Shader/DeferredPBR/DeferredPBR.fs
+++ Assets/Shader/DeferredPBR/DeferredPBR.fs
@@ -278,15 +278,15 @@ float ShadowCalculation(vec4 fragPosLightSpace,vec3 n,vec3 lightDir)
 
     float shd = 0.0;
     vec2 texelSize = 1.0 / textureSize(shadowMap, 0);
-    for(int x = -1; x <= 1; ++x)
+    for(int x = -4; x <= 4; ++x)
     {
-        for(int y = -1; y <= 1; ++y)
+        for(int y = -4; y <= 4; ++y)
         {
             float pcfDepth = texture(shadowMap, projCoords.xy + vec2(x, y) * texelSize).r; 
             shd += currentDepth - bias > pcfDepth  ? 1.0 : 0.0;        
         }    
     }
-    shd /= 9.0;
+    shd /= 81.0;
     if(projCoords.z > 1.0)return 0.0f;
     return shd;
 }  
